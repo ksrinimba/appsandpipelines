0101ee923dd0289b3730bfdb8f664dc2bce6f443
ksrinimba <ksrinimba@gmail.com>
2023-06-14 21:37:36 -0700

    added other stuff

--- misc/gen-pipe.sh
+++ misc/gen-pipe.sh
@@ -9,7 +9,8 @@
     fileName="$appName-$pipeName"
     n=$(($j % 10))    
     groupName="group$n"
-    jqStr=".application = \"$appName\"|.name = \"$pipeName\"|.triggers[0].source = \"$pipeName\""
+    #jqStr=".application = \"$appName\"|.name = \"$pipeName\"|.triggers[0].source = \"$pipeName\""
+    jqStr=".application = \"$appName\"|.name = \"$pipeName\"|.triggers[0].source = \"$groupName\""
     #jqStr=".application = \"$appName\"|.name = \"$pipeName\"|.triggers[0].source = \"$groupName\"|.roles[0] = \"$pipeName\""
     cat pipe-with-role.json | jq "$jqStr" > ../pipelines/$fileName.json
   done  
--- misc/load-apps.sh
+++ misc/load-apps.sh
@@ -1,7 +1,7 @@
 for i in {1..10}
 do
    echo "Loading app $i"
-   spin -k app save -f ../apps/loadapp$i.json --config config --retry-timeout 1200 &
+   spin -k app save -f ../apps/loadapp$i.json  --retry-timeout 1200 &
 done
 echo sleeping....
 sleep 3600
